876e7147a352bedd13a4483fe57a130ea528a683
prasannabhat97 <37222040+prasannabhat97@users.noreply.github.com>
2019-12-30 07:36:26 +0530

    Add files via upload

--- Project Files/demo.js
+++ Project Files/demo.js
@@ -1,3 +1,10 @@
 function selected(element) {
     element.classList.toggle("highlight");
   }
+function validate(){
+  var ele=document.getElementsByClassName("btn").value;
+for(i in ele){
+      
+      window.alert(i)
+}
+}
